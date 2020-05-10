fdabc6c8b4239587b9b5b799cd411fcc3322bb70
raghavchandak <raghavchandak25@gmail.com>
2020-05-10 17:29:34 +0530

    Created clean architecture folder set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:nih_laundro/Screens/DisplayScreen.screen.dart';
-import 'Screens/HomePage.screen.dart';
-import 'package:nih_laundro/Backend/shared_pref.dart';
 
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  Widget page = Home();
-  @override
-  void initState()
-  {
-    super.initState();
-    setScreen();
-  }
-
-  void setScreen() async
-  {
-    Widget currentPage = await StorageService().isTotal() ? DisplayScreen() : Home();
-    setState(() {
-      page = currentPage; 
-    });
-  }
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: page,
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
